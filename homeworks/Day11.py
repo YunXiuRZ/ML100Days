be6eb57a3e8dea9c766e7d764d8ccc5041a346bd
YunXiuRZ <70698588+YunXiuRZ@users.noreply.github.com>
2021-01-26 20:20:28 +0800

    Create Day11.py

---
+++ homeworks/Day11.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+#topic
+q_df = pd.DataFrame(
+    [['male', 'teacher'], 
+    ['male', 'engineer'], 
+    ['female', None], 
+    ['female', 'engineer']],
+    columns=['Sex','Profession'])
+
+#1
+q_df.fillna('other')
+print(q_df)
+
+#2
+q_df_sex = pd.get_dummies(q_df['Sex'])
+q_df = pd.concat([q_df, q_df_sex], axis = 1)
+q_df_profession = pd.get_dummies(q_df['Profession'])
+q_df = pd.concat([q_df, q_df_profession], axis = 1)
+print(q_df)
